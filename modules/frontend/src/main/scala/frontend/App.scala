846dbb2cfcbbcfe0afb47bb4f230709c2eadf52b
Marek Kadek <kadek.marek@gmail.com>
2017-04-23 23:03:08 +0200

    meh

--- modules/frontend/src/main/scala/frontend/App.scala
+++ modules/frontend/src/main/scala/frontend/App.scala
@@ -1,62 +1,54 @@
 package frontend
 
-import org.scalajs.dom.document
+import org.scalajs.dom.{document, html}
 import japgolly.scalajs.react._
+import japgolly.scalajs.react.extra._
+import japgolly.scalajs.react.internal.CatsReactState.ReactS
 import japgolly.scalajs.react.vdom.html_<^._
-import japgolly.scalajs.react.raw.Props
+import japgolly.scalajs.react.CatsReact._
 
 import scala.scalajs.js
 import scala.scalajs.js.JSApp
+import cats.implicits._
 
 object App extends JSApp {
 
-  final case class TodoItem(id: Long, name: String)
-
-  final case class TodoListProps(items: List[TodoItem], onRemove: Long => Callback)
-  val TodoList = ScalaComponent
-    .builder[TodoListProps]("TodoList")
-    .render_P { props =>
-      def createItem(item: TodoItem) = <.li(item.name, <.button(" [x]", item.id, ^.onClick --> props.onRemove(item.id)) )
-      <.ul(props.items.map(createItem): _*)
-    }
+  val TodoList = ScalaComponent.builder[List[String]]("TodoList")
+    .render_P(items =>
+      <.ul(items.map(<.li(_)): _*))
     .build
 
-  //
+  case class State(items: List[String], text: String)
 
-  final case class State(items: List[TodoItem], text: String)
-  class Backend($ : BackendScope[Unit, State]) {
-    var nextId = 1
+  val ST = ReactS.Fix[State]                              // Let's use a helper so that we don't have to specify the
+  //   state type everywhere.
 
-    def onChange(e: ReactEventFromInput) = {
-      val newValue = e.target.value
-      $.modState(_.copy(text = newValue))
-    }
+  def acceptChange(e: ReactEventFromInput) =
+    ST.mod(_.copy(text = e.target.value))                 // A pure state modification. State value is provided when run.
 
-    def handleSubmit(e: ReactEventFromInput) =
-      e.preventDefaultCB >>
-      $.modState(s => State(s.items :+ TodoItem(nextId, s.text), "")) >>
-      Callback { nextId += 1 }
+  def handleSubmit(e: ReactEventFromInput) = (
+    ST.retM(e.preventDefaultCB)                           // Lift a Callback effect into a shape that allows composition
+      //   with state modification.
+      >>                                                    // Use >> to compose. It's flatMap (>>=) that ignores input.
+      ST.mod(s => State(s.items :+ s.text, "")).liftCB      // Here we lift a pure state modification into a shape that
+    )                                                       //   allows composition with Callback effects.
 
-    def onRemove(id: Long) = $.modState(s => s.copy(items = s.items.filter(x => x.id != id)))
-
-    def render(state: State) =
-      <.div(
-        <.h3("TODO LIST"),
-        TodoList(TodoListProps(state.items, onRemove)),
-        <.form(^.onSubmit ==> handleSubmit,
-               <.input(^.onChange ==> onChange, ^.value := state.text),
-               <.button("Add #", state.items.length + 1))
-      )
-  }
-
-  val TodoApp = ScalaComponent
-    .builder[Unit]("TodoApp")
+  val TodoApp = ScalaComponent.builder[Unit]("TodoApp")
     .initialState(State(Nil, ""))
-    .renderBackend[Backend]
-    .build
+    .renderS(($, s) =>
+      <.div(
+        <.h3("TODO"),
+        TodoList(s.items),
+        <.form(^.onSubmit ==> $.runStateFn(handleSubmit), // runState runs a state monad and applies the result.
+          <.input(                                        // runStateFn is similar but takes a function-to-a-state-monad.
+            ^.onChange ==> $.runStateFn(acceptChange),    // In these cases, the function will be fed the JS event.
+            ^.value := s.text),
+          <.button("Add #", s.items.length + 1)))
+    ).build
 
   def main(): Unit = {
     val appDiv = document.getElementById("app")
+
     TodoApp().renderIntoDOM(appDiv)
   }
 }
